db2f9ae26e288cdf960a22ce7236481867ed89fe
Jamil Villones <50472091+jamvillones@users.noreply.github.com>
2024-01-13 03:49:51 +0800

    create customer form
    separate form between customers view and registration form

---
+++ POS/Forms/Create_Customer_Form.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POS.Forms
+{
+    public partial class Create_Customer_Form : Form
+    {
+        public Create_Customer_Form()
+        {
+            InitializeComponent();
+        }
+    }
+}
